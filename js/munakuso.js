b34543c1f94bed88b904b96a7e7beff38d5d72ac
alphonse-av98 <av98siki@gmail.com>
2024-08-04 03:43:31 +0900

    Change face emotion.

--- js/munakuso.js
+++ js/munakuso.js
@@ -13,7 +13,7 @@ const meter_options = {
 function drawChart() {
     meter_data = google.visualization.arrayToDataTable([
         ['Label', 'Value'],
-        ['😑', 0],
+        ['😐', 0],
     ]);
     chart = new google.visualization.Gauge(document.getElementById('meter'));
     chart.draw(meter_data, meter_options);
@@ -22,26 +22,26 @@ function drawChart() {
 function modMunakuso(value) {
     let next_value = meter_data.getValue(0, 1) + value;
     meter_data.setValue(0, 1, next_value < 100 ? next_value > 0 ? next_value : 0 : 100);
-    if (next_value < 25) {
-        meter_data.setValue(0, 0, '😑');
+    if (next_value > 0 && next_value < 25) {
+        meter_data.setValue(0, 0, '😐');
     } else if (next_value >= 25 && next_value < 50) {
         meter_data.setValue(0, 0, '🙁');
     } else if (next_value >= 50 && next_value < 75) {
-        meter_data.setValue(0, 0, '😦');
-    } else if (next_value >= 75 && next_value < 90) {
         meter_data.setValue(0, 0, '😠');
-    } else if (next_value >= 90 && next_value < 100) {
+    } else if (next_value >= 75 && next_value < 90) {
         meter_data.setValue(0, 0, '😡');
-    } else if (next_value >= 100) {
+    } else if (next_value >= 90 && next_value < 100) {
         meter_data.setValue(0, 0, '🤬');
+    } else if (next_value >= 100) {
+        meter_data.setValue(0, 0, '💥');
     } else {
-        meter_data.setValue(0, 0, '😑');
+        meter_data.setValue(0, 0, '😐');
     }
     chart.draw(meter_data, meter_options);
 }
 
 function resetMunakuso() {
-    meter_data.setValue(0, 0, '😑');
+    meter_data.setValue(0, 0, '😐');
     meter_data.setValue(0, 1, 0);
     chart.draw(meter_data, meter_options);
 
